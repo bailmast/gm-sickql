03ae381c29457e8f251bd6fea350fd3c59cb7f56
Bailmast <57112957+bailmast@users.noreply.github.com>
2024-02-08 13:34:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# SickQL
+
+Garry's Mod unique database control interface that supports `MySQLite`, `MySQLOO` & `TMySQL`.
+
+# Usage
+
+```lua
+local sickDB = SickQL:New('MySQLite')
+
+function sickDB:OnConnected()
+  print("MySQLite is boring! But it's connected!")
+
+  local q = self:Query('SELECT sqlite_version();')
+
+  function q:OnSuccess(data)
+    print('Your beatifuly and boring MySQLite version is ' .. data[1]['sqlite_version()'])
+  end
+
+  function q:OnError(why)
+    print('Something went wrong! Why: ' .. why)
+  end
+end
+
+function sickDB:OnConnectionFailed(why)
+  -- unreachable
+end
+
+sickDB:Connect()
+```
+
+```lua
+-- Same code for MySQLOO! Just don't forget to change vendor!
+local sickDB = SickQL:New('TMySQL', '127.0.0.1', 3306, 'root', 'root', 'sick')
+
+function sickDB:OnConnected()
+  print("TMySQL is for happy people :)")
+
+  local q = self:Query('SELECT version();')
+
+  function q:OnSuccess(data)
+    print("Hi! I'm TMySQL and my version is " .. data[1]['sqlite_version()'])
+  end
+
+  function q:OnError(why)
+    print('Something went wrong! Why: ' .. why)
+  end
+end
+
+function sickDB:OnConnectionFailed(why)
+  print('Connection failed: ' .. why)
+end
+
+sickDB:Connect()
+```
---
+++ sv_sickql.lua
@@ -0,0 +1,174 @@
+SickQL = SickQL or {}
+
+local CURRENT_VERSION = 240201
+if SickQL._VERSION and (SickQL._VERSION <= CURRENT_VERSION) then return end
+SickQL._VERSION = CURRENT_VERSION
+
+if util.IsBinaryModuleInstalled('mysqloo') then
+  require('mysqloo')
+end
+
+if util.IsBinaryModuleInstalled('tmysql4') then
+  require('tmysql4')
+  SickQL.TMySQL_HookFormat = 'SickQL::TMySQLPolling(%s)'
+  SickQL.TMySQL_LastConnection = SickQL.TMySQL_LastConnection or 0
+end
+
+SickQL.Implementations = SickQL.Implementations or {
+  ['MySQLite'] = {
+    Create = function(conn)
+      return nil --[[ vdb ]], nil --[[ err ]]
+    end,
+    Connect = function(db)
+      db:OnConnected()
+    end,
+    Query = function(vdb, q)
+      local res = sql.Query(q.String)
+      if res ~= false then
+        local data = res or {}
+        q:OnSuccess(data)
+      else
+        q:OnError(sql.LastError())
+      end
+    end,
+    Disconnect = function(vdb, conn)
+    end
+  },
+  ['MySQLOO'] = {
+    Create = function(conn)
+      return mysqloo.connect(conn.Host, conn.Username, conn.Password, conn.DatabaseName, conn.Port), nil --[[ err ]]
+    end,
+    Connect = function(db)
+      local vdb = db.VendorDatabase
+
+      function vdb:onConnected()
+        db:OnConnected()
+      end
+
+      function vdb:onConnectionFailed(why)
+        db:OnConnectionFailed(why)
+      end
+
+      vdb:connect()
+    end,
+    Query = function(vdb, q)
+      local vq = vdb:query(q.String)
+
+      function vq:onSuccess(data)
+        q:OnSuccess(data)
+      end
+
+      function vq:onError(why)
+        q:OnError(why)
+      end
+
+      vq:start()
+    end,
+    Disconnect = function(vdb, conn)
+      vdb:disconnect()
+    end
+  },
+  ['TMySQL'] = {
+    Create = function(conn)
+      return tmysql.Create(conn.Host, conn.Username, conn.Password, conn.DatabaseName, conn.Port)
+    end,
+    Connect = function(db)
+      local _, err = db.VendorDatabase:Connect()
+      if err ~= nil then
+        db:OnConnectionFailed(err)
+        return
+      end
+
+      db:OnConnected()
+
+      SickQL.TMySQL_LastConnection = SickQL.TMySQL_LastConnection + 1
+      db.ConnectionInfo.TMySQLConnection = SickQL.TMySQL_LastConnection
+
+      hook.Add('Think', SickQL.TMySQL_HookFormat:format(SickQL.TMySQL_LastConnection), function()
+        db.VendorDatabase:Poll()
+      end)
+    end,
+    Query = function(vdb, q)
+      vdb:Query(q.String, function(res)
+        if res[1].error ~= nil then
+          q:OnError(res[1].error)
+          return
+        end
+
+        q:OnSuccess(res[1].data)
+      end)
+    end,
+    Disconnect = function(vdb, conn)
+      hook.Remove('Think', SickQL.TMySQL_HookFormat:format(conn.TMySQLConnection))
+      vdb:Disconnect()
+    end
+  }
+}
+
+local DATABASE_META = {
+  OnConnected = function(db) end,
+  OnConnectionFailed = function(db, why) end
+}
+DATABASE_META.__index = DATABASE_META
+
+---Creates and prepares your database instance before connecting to it.
+---@param vendor string
+---@param host? string
+---@param port? number
+---@param username? string
+---@param password? string
+---@param database? string
+---@return table
+---@return string?
+function SickQL:New(vendor, host, port, username, password, database)
+  local db = setmetatable({
+    Implementation = self.Implementations[vendor],
+    VendorDatabase = nil,
+    ConnectionInfo = {
+      Host = host,
+      Port = port,
+      Username = username,
+      Password = password,
+      DatabaseName = database
+    }
+  }, DATABASE_META)
+
+  local vdb, err = db.Implementation.Create(db.ConnectionInfo)
+  db.VendorDatabase = vdb
+
+  return db, err
+end
+
+---Connect database itself.
+---@return table
+function DATABASE_META:Connect()
+  self.Implementation.Connect(self)
+  return self
+end
+
+---Disconnect from database.
+function DATABASE_META:Disconnect()
+  self.Implementation.Disconnect(self.VendorDatabase, self.ConnectionInfo)
+end
+
+local QUERY_META = {
+  OnSuccess = function(q, data) end,
+  OnError = function(q, why) end
+}
+QUERY_META.__index = QUERY_META
+
+---Prepare query so you can change `OnSuccess` and `OnError` functions.
+---@param str string
+---@return table
+function DATABASE_META:Query(str)
+  return setmetatable({
+    String = str,
+    Database = self
+  }, QUERY_META)
+end
+
+---Start query itself.
+function QUERY_META:Start()
+  local db = self.Database
+  db.Implementation.Query(db.VendorDatabase, self)
+end
