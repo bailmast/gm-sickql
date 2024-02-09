5588ccfc00c7fc5664a9e7b5d2845ac978cb62c5
Bailmast <57112957+bailmast@users.noreply.github.com>
2024-02-09 04:05:58 +0300

    fixed possible error in `TMySQL`

--- sv_sickql.lua
+++ sv_sickql.lua
@@ -1,6 +1,6 @@
 SickQL = SickQL or {}
 
-local CURRENT_VERSION = 240201
+local CURRENT_VERSION = 240209
 if SickQL._VERSION and (SickQL._VERSION <= CURRENT_VERSION) then return end
 SickQL._VERSION = CURRENT_VERSION
 
@@ -10,6 +10,7 @@ end
 
 if util.IsBinaryModuleInstalled('tmysql4') then
   require('tmysql4')
+
   SickQL.TMySQL_HookFormat = 'SickQL::TMySQLPolling(%s)'
   SickQL.TMySQL_LastConnection = SickQL.TMySQL_LastConnection or 0
 end
@@ -24,15 +25,15 @@ SickQL.Implementations = SickQL.Implementations or {
     end,
     Query = function(vdb, q)
       local res = sql.Query(q.String)
-      if res ~= false then
-        local data = res or {}
-        q:OnSuccess(data)
-      else
+      if res == false then
         q:OnError(sql.LastError())
+        return
       end
+
+      local data = res or {}
+      q:OnSuccess(data)
     end,
-    Disconnect = function(vdb, conn)
-    end
+    Disconnect = function(vdb, conn) end
   },
   ['MySQLOO'] = {
     Create = function(conn)
@@ -73,8 +74,8 @@ SickQL.Implementations = SickQL.Implementations or {
       return tmysql.Create(conn.Host, conn.Username, conn.Password, conn.DatabaseName, conn.Port)
     end,
     Connect = function(db)
-      local _, err = db.VendorDatabase:Connect()
-      if err ~= nil then
+      local success, err = db.VendorDatabase:Connect()
+      if not success then
         db:OnConnectionFailed(err)
         return
       end
@@ -90,12 +91,18 @@ SickQL.Implementations = SickQL.Implementations or {
     end,
     Query = function(vdb, q)
       vdb:Query(q.String, function(res)
-        if res[1].error ~= nil then
-          q:OnError(res[1].error)
+        res = res[1]
+        if res == nil then
+          q:OnError('Result is nil!')
           return
         end
 
-        q:OnSuccess(res[1].data)
+        if res.error ~= nil then
+          q:OnError(res.error)
+          return
+        end
+
+        q:OnSuccess(res.data)
       end)
     end,
     Disconnect = function(vdb, conn)
@@ -112,7 +119,7 @@ local DATABASE_META = {
 DATABASE_META.__index = DATABASE_META
 
 ---Creates and prepares your database instance before connecting to it.
----@param vendor string
+---@param impl string
 ---@param host? string
 ---@param port? number
 ---@param username? string
@@ -120,9 +127,9 @@ DATABASE_META.__index = DATABASE_META
 ---@param database? string
 ---@return table
 ---@return string?
-function SickQL:New(vendor, host, port, username, password, database)
+function SickQL:New(impl, host, port, username, password, database)
   local db = setmetatable({
-    Implementation = self.Implementations[vendor],
+    Implementation = self.Implementations[impl],
     VendorDatabase = nil,
     ConnectionInfo = {
       Host = host,
