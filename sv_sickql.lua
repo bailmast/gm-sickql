3ea2280043f7077934dbd6e09beb26370ba30d53
Bailmast <57112957+bailmast@users.noreply.github.com>
2024-03-20 01:14:02 +0300

    `sql.SQLStr` quotes (skill) issue

--- sv_sickql.lua
+++ sv_sickql.lua
@@ -12,12 +12,8 @@ end
 
 SickQL.Implementations = SickQL.Implementations or {
   ['SQLite'] = {
-    Create = function(conn)
-      return nil --[[ vdb ]], nil --[[ err ]]
-    end,
-    Connect = function(db)
-      db:OnConnected()
-    end,
+    Create = function(conn) return nil --[[ vdb ]], nil --[[ err ]] end,
+    Connect = function(db) db:OnConnected() end,
     Query = function(vdb, q)
       local res = sql.Query(q.String)
       if res == false then
@@ -29,9 +25,7 @@ SickQL.Implementations = SickQL.Implementations or {
       q:OnSuccess(data)
     end,
     Disconnect = function(vdb, conn) end,
-    Escape = function(vdb, str)
-      return sql.SQLStr(str)
-    end
+    Escape = function(vdb, str) return sql.SQLStr(str, true) end,
   },
   ['MySQLOO'] = {
     Create = function(conn)
@@ -63,12 +57,8 @@ SickQL.Implementations = SickQL.Implementations or {
 
       vq:start()
     end,
-    Disconnect = function(vdb, conn)
-      vdb:disconnect()
-    end,
-    Escape = function(vdb, str)
-      return vdb:escape(str)
-    end
+    Disconnect = function(vdb, conn) vdb:disconnect() end,
+    Escape = function(vdb, str) return vdb:escape(str) end,
   },
   ['TMySQL'] = {
     Create = function(conn)
@@ -76,34 +66,37 @@ SickQL.Implementations = SickQL.Implementations or {
     end,
     Connect = function(db)
       local conn = db.ConnectionInfo
-      local hookTag = SickQL.TMySQL_HookFormat:format(conn.Host, conn.Username, conn.Password, conn.DatabaseName, conn.Port)
+      local hookTag = string.format(SickQL.TMySQL_HookFormat, conn.Host, conn.Username, conn.Password, conn.DatabaseName, conn.Port)
+
       if hook.GetTable()['Think'][hookTag] ~= nil then
         db:OnConnectionFailed("TMySQL can't handle multiple connections to one database!")
+
         return
       end
 
       local success, err = db.VendorDatabase:Connect()
       if not success then
         db:OnConnectionFailed(err)
+
         return
       end
 
       db:OnConnected()
 
-      hook.Add('Think', hookTag, function()
-        db.VendorDatabase:Poll()
-      end)
+      hook.Add('Think', hookTag, function() db.VendorDatabase:Poll() end)
     end,
     Query = function(vdb, q)
       vdb:Query(q.String, function(res)
         res = res[1]
         if res == nil then
           q:OnError('Result is nil!')
+
           return
         end
 
         if res.error ~= nil then
           q:OnError(res.error)
+
           return
         end
 
@@ -114,27 +107,22 @@ SickQL.Implementations = SickQL.Implementations or {
       hook.Remove('Think', SickQL.TMySQL_HookFormat:format(conn.Host, conn.Username, conn.Password, conn.DatabaseName, conn.Port))
       vdb:Disconnect()
     end,
-    Escape = function(vdb, str)
-      return vdb:Escape(str)
-    end
+    Escape = function(vdb, str) return vdb:Escape(str) end,
   }
 }
 
-local DATABASE_META = {
-  OnConnected = function(db) end,
-  OnConnectionFailed = function(db, why) end
-}
+local DATABASE_META = {}
 DATABASE_META.__index = DATABASE_META
 
----Creates and prepares your database instance before connecting to it.
----@param impl string
----@param host? string
----@param port? number
----@param username? string
----@param password? string
----@param database? string
----@return table
----@return string?
+--- Creates and prepares your database instance before connecting to it.
+--- @param impl string
+--- @param host? string
+--- @param port? number
+--- @param username? string
+--- @param password? string
+--- @param database? string
+--- @return table
+--- @return string?
 function SickQL:New(impl, host, port, username, password, database)
   local db = setmetatable({
     Implementation = self.Implementations[impl],
@@ -154,34 +142,32 @@ function SickQL:New(impl, host, port, username, password, database)
   return db, err
 end
 
----Connect database itself.
----@return table
+--- Connect database itself.
+--- @return table
 function DATABASE_META:Connect()
   self.Implementation.Connect(self)
+
   return self
 end
 
----Disconnect from database.
+--- Disconnect from database.
 function DATABASE_META:Disconnect()
   self.Implementation.Disconnect(self.VendorDatabase, self.ConnectionInfo)
 end
 
----Escape a string using a method provided by the database vendor.
----@param str string
----@return string
+--- Escape a string using a method provided by the database vendor.
+--- @param str string
+--- @return string
 function DATABASE_META:Escape(str)
   return self.Implementation.Escape(self.VendorDatabase, str)
 end
 
-local QUERY_META = {
-  OnSuccess = function(q, data) end,
-  OnError = function(q, why) end
-}
+local QUERY_META = {}
 QUERY_META.__index = QUERY_META
 
----Prepare query so you can change `OnSuccess` and `OnError` functions.
----@param str string
----@return table
+--- Prepare query so you can change `OnSuccess` and `OnError` functions.
+--- @param str string
+--- @return table
 function DATABASE_META:Query(str)
   return setmetatable({
     String = str,
@@ -189,22 +175,25 @@ function DATABASE_META:Query(str)
   }, QUERY_META)
 end
 
----@param cback fun(q: Query, data: table)
----@return table
+--- @param cback fun(q: Query, data: table)
+--- @return table
 function QUERY_META:SetOnSuccess(cback)
   self.OnSuccess = cback
+
   return self
 end
 
----@param cback fun(q: Query, why: string)
----@return table
+--- @param cback fun(q: Query, why: string)
+--- @return table
 function QUERY_META:SetOnError(cback)
   self.OnError = cback
+
   return self
 end
 
----Start query itself.
+--- Start query itself.
 function QUERY_META:Start()
   local db = self.Database
+
   db.Implementation.Query(db.VendorDatabase, self)
 end
